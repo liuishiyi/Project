97fea70b0d862891b9235cd9965946f4fa8857b8
ShiYi_workstation <shiyi@workstation.com>
2019-11-19 22:30:28 +0800

    11-19

--- A Python Interpreter Written in Python/Interpreter.py
+++ A Python Interpreter Written in Python/Interpreter.py
@@ -1,2 +1,96 @@
 # http://aosabook.org/en/500L/a-python-interpreter-written-in-python.html
 
+# what_to_execute = {  # directory
+#         "instructions": [("LOAD_VALUE", 0),  # the first number
+#                          ("LOAD_VALUE", 1),  # the second number
+#                          ("ADD_TWO_VALUES", None),
+#                          ("PRINT_ANSWER", None)],
+#         "number": [7, 5]
+# }
+
+what_to_execute = {
+        "instructions": [("LOAD_VALUE", 0),
+                         ("STORE_NAME", 0),
+                         ("LOAD_VALUE", 1),
+                         ("STORE_NAME", 1),
+                         ("LOAD_NAME", 0),
+                         ("LOAD_NAME", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)
+                        ],
+        "numbers": [1, 2],
+        "names": ["a", "b"]
+}
+
+
+class Interpreter:
+    def __init__(self):
+        self.stack = []
+        self.environment = {}
+
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.environment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.environment[name]
+        self.stack.append(val)
+
+    def LOAD_VALUE(self, number):
+        self.stack.append(number)
+
+    def PRINT_ANSWER(self):
+        answer = self.stack.pop()
+        print(answer)
+
+    def ADD_TWO_VALUES(self):
+        first_num = self.stack.pop()
+        second_num = self.stack.pop()
+        total = first_num + second_num
+        self.stack.append(total)
+
+    def parse_argument(self, instruction, argument, what_to_execute):
+        """Understand what the argument to each instruction means."""
+        numbers = ["LOAD_VALUE"]
+        names = ["LOAD_NAME", "STORE_NAME"]
+
+        if instruction in numbers:
+            argument = what_to_execute["numbers"][argument]
+        elif instruction in names:
+            argument = what_to_execute["names"][argument]
+
+        return argument
+
+    def run_code(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_argument(instruction, argument, what_to_execute)
+
+            if instruction == "LOAD_VALUE":
+                self.LOAD_VALUE(argument)
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VALUES()
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(argument)
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(argument)
+
+    def execute(self, what_to_execute):
+        instructions = what_to_execute["instructions"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            argument = self.parse_argument(instruction, argument, what_to_execute)
+            bytecode_method = getattr(self, instruction)
+            if argument is None:
+                bytecode_method()
+            else:
+                bytecode_method(argument)
+
+
+interpreter = Interpreter()
+# interpreter.run_code(what_to_execute)
+interpreter.execute(what_to_execute)
+
